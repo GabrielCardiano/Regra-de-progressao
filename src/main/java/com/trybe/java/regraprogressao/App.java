5f9c00816220869f69eee5474e064def02c7bb26
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2024-01-19 20:51:11 -0300

    feat: req 1 - cria cadastro de atividades avaliativas

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,8 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -7,7 +10,39 @@ public class App {
 
   /**
    * Metodo main.
+   *
+   * @param args the input arguments
    */
   public static void main(String[] args) {
+    registerEvaluativeActivity();
+  }
+
+  /**
+   * Register evaluative activity.
+   */
+  public static void registerEvaluativeActivity() {
+    Scanner scanner = new Scanner(System.in);
+
+    int countActivities = 1;
+    HashMap<String, Integer> finalGrade = new HashMap<String, Integer>();
+
+    System.out.print("Digite a quantidade de atividades para cadastrar: ");
+    int activityQnt = scanner.nextInt();
+    scanner.nextLine();
+
+    while (countActivities <= activityQnt) {
+      System.out.print("Digite o nome da atividade " + countActivities + ": ");
+      String nome = scanner.nextLine();
+
+      System.out.print("Digite o peso da atividade " + countActivities + ": ");
+      int peso = scanner.nextInt();
+      scanner.nextLine();
+
+      finalGrade.put(nome, peso);
+      countActivities++;
+    }
+
+    System.out.println(finalGrade);
+    scanner.close();
   }
 }
