7fcfea80713115a4da648db93403a4c61b5b9c4b
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2024-01-20 08:55:07 -0300

    feat: req 3 - add verificação da soma dos pesos e refatora - substituindo nextInt

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -14,35 +14,38 @@ public class App {
    * @param args the input arguments
    */
   public static void main(String[] args) {
-    cadastroDeAtividadesAvaliativas();
+    registerEvaluativeActivity();
   }
 
   /**
    * Register evaluative activity.
    */
-  public static void cadastroDeAtividadesAvaliativas() {
+  public static void registerEvaluativeActivity() {
     Scanner scanner = new Scanner(System.in);
 
     int countActivities = 1;
+    int totalWeight = 0;
 
     // ------- Cadastro de atividade -----------
     System.out.print("Digite a quantidade de atividades para cadastrar: ");
-    int activityQnt = scanner.nextInt();
-    scanner.nextLine();
+    int activityQnt = Integer.parseInt(scanner.nextLine());
 
     while (countActivities <= activityQnt) {
       System.out.print("Digite o nome da atividade " + countActivities + ": ");
       String nome = scanner.nextLine();
 
       System.out.print("Digite o peso da atividade " + countActivities + ": ");
-      int peso = scanner.nextInt();
-      scanner.nextLine();
+      int weight = Integer.parseInt(scanner.nextLine());
+      totalWeight += weight;
 
       System.out.print("Digite a nota obtida para " + nome + ": ");
-      int nota = scanner.nextInt();
-      scanner.nextLine();
+      int grade = Integer.parseInt(scanner.nextLine());
 
       countActivities++;
+    }
+
+    if (totalWeight != 100) {
+      System.out.println("A soma dos pesos é diferente de 100!");
     }
 
     scanner.close();
